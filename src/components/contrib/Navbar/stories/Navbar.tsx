0d933dd18213e37f5feb3fbcfb0b8465880d0d79
Heecheon Kim <hckim893@gmail.com>
2020-01-01 19:43:06 +0900

    Add navbar

---
+++ src/components/contrib/Navbar/stories/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Navbar } from '../index';
+
+export default {
+  title: 'Navbar',
+};
+
+export const empty = () => <Navbar />;
--- src/components/contrib/Navbar/style.ts
+++ src/components/contrib/Navbar/style.ts
@@ -1,6 +1,13 @@
 import styled from 'styled-components';
 
+import { Colors } from 'components/styles/colors';
+
 export const Navbar = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
   width: 100%;
-  background-color: black;
+  height: 4rem;
+  background-color: ${Colors.Brown};
+  color: ${Colors.Grey100};
 `;
