3bd4ddec9fdb8c682395b20d9978c7dfa857aa2d
Heecheon Kim <hckim893@gmail.com>
2020-01-04 16:27:11 +0900

    <fix> Update navbar style

--- src/components/contrib/Navbar/style.ts
+++ src/components/contrib/Navbar/style.ts
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
 
+import { Button } from 'components/common';
 import { Colors } from 'styles/colors';
 import { Size, Weight } from 'styles/typograhpy';
 import { flexStyle } from 'styles/flex';
 
 export const NavbarTitle = styled.h1`
+  padding: 1rem 1.6rem;
   font-size: ${Size.Header};
   font-weight: ${Weight.Bold};
 `;
@@ -14,10 +16,12 @@ export const NavbarActions = styled.div`
   height: 100%;
 `;
 
-export const NavbarButton = styled.button`
-  font-size: ${Size.Body};
-  font-weight: ${Weight.Medium};
+export const NavbarButton = styled(Button)`
+  height: 100%;
 `;
+NavbarButton.defaultProps = {
+  theme: 'brown',
+};
 
 export const Navbar = styled.nav`
   ${flexStyle({ alignItems: 'center' })}
@@ -25,7 +29,7 @@ export const Navbar = styled.nav`
   top: 0;
   left: 0;
   width: 100%;
-  height: 4rem;
+  height: 5rem;
   background-color: ${Colors.Brown};
-  color: ${Colors.Grey100};
+  color: ${Colors.WhiteText};
 `;
